e240b25bbc64573684f518280952c5a29e30b129
unknown <UFG@REDES-32.ufg.local>
2024-11-12 20:12:10 -0600

    practica 2 Unidad 4

--- fala_dbk_20241105/app/Http/Controllers/AuthController.php
+++ fala_dbk_20241105/app/Http/Controllers/AuthController.php
@@ -2,39 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use hash;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
 
 class AuthController extends Controller
 {
-    //
-    public function regiter(Request $request)
-    {$validateData = $request->validate([
-        'name'=>['required','string','max:255'],
-        'email'=>['required','string','email:','max:255','unique:users'],
-        'password'=>['required','string','min:8','max:20'],
-    ]);
+    
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate(rules: [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'max:20'],
+        ]);
 
-    $user = User::create([
-        'name'=> $validateData['name'],
-        'email'=> $validateData['email'],
-        'password'=> Hash::make($validateData['password']),
-    ]);
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
 
-    $token = $user->createToken('auth_token')->PlainTextToken;
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            "Success" => true,
+            "errors" => [
+                "code" => 0,
+                "msg" => ""
+            ],
+            "data" => [
+                "access_token" => $token,
+                "token_type" => "Bearer"
+            ],
+            "msg" => "Usuario creado satisfactoriamente",
+            "count" => 1
+        ]);
+    }
+
+    public function login(Request $request){
+        if(!Auth::attempt($request->only("email","password"))){
+            return response()->json([
+                "success"=> false,
+                "errors"=>[
+                    "code"=>401,
+                    "msg"=> "No se encontraron las credenciales"
+                ],
+                "data"=>"",
+                "count"=> 0
+                ], 401);
+    }
+    $user = User::where("email", $request->email)->firstOrFail();
+    $token = $user->createToken("auth_token")->plainTextToken;
 
     return response()->json([
-        "succes"=> true,
+        "success"=> true,
         "errors"=>[
-            "code"=>0,
-            "msg"=>""
-        ],
-        "data"=>[
-            "acces_token"=> $token,
-            "token_type" => "Bearer"
-        ],
-        "msg"=>"Usuario creado satisfactoriamente",
-        "count"=>1
-    ]);
+            "code"=>200,
+            "msg"=> ""
+            ],
+            "data"=>[
+                "access_token"=> $token,
+                "token_type"=> "Bearer"
+                ],
+            "msg"=>"Ha iniciado sesión correctamente",
+            "count"=> 1
+            ],200);
+    }
+
+    public function me(Request $request){
+        return response()->json([
+            "success"=> true,
+            "errors"=>[
+                "code"=>200,
+                "msg"=> ""
+                ],
+                "data"=>$request->user(),
+                "count"=> 1
+            ],200);
+    }
 }
