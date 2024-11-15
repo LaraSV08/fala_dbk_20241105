3b84c4d637c97e992cb1364647a3353808111d56
LaraSV08 <140665251+LaraSV08@users.noreply.github.com>
2024-11-15 00:40:41 -0600

    Update

--- fala_dbk_20241105/app/Http/Controllers/AuthController.php
+++ fala_dbk_20241105/app/Http/Controllers/AuthController.php
@@ -2,85 +2,87 @@
 
 namespace App\Http\Controllers;
 
-use hash;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
-
 
 class AuthController extends Controller
 {
-    
-    public function register(Request $request)
-    {
-        $validatedData = $request->validate(rules: [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'max:20'],
+    //
+    public function register(Request $request){
+        $validatedData = $request->validate([
+            'name'=>['required','string','max:255'],
+            'email'=>['required','string','email','max:255','unique:users'],
+            'password'=>['required','string','min:8','max:20'],
         ]);
 
         $user = User::create([
-            'name' => $validatedData['name'],
-            'email' => $validatedData['email'],
-            'password' => Hash::make($validatedData['password']),
+            'name'=> $validatedData['name'],
+            'email'=> $validatedData['email'],
+            'password'=> Hash::make($validatedData['password']),
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
-            "Success" => true,
-            "errors" => [
-                "code" => 0,
-                "msg" => ""
+            "success"=> true,
+            "errors"=>[
+                "code"=>0,
+                "msg"=>""
             ],
-            "data" => [
-                "access_token" => $token,
+            "data"=>[
+                "access_token"=>$token,
                 "token_type" => "Bearer"
             ],
-            "msg" => "Usuario creado satisfactoriamente",
-            "count" => 1
+            "msg"=>"Usuario creado satisfactoriamente",
+            "count"=>1
         ]);
     }
 
-    public function login(Request $request){
-        if(!Auth::attempt($request->only("email","password"))){
-            return response()->json([
+
+
+    public function login(Request $request) {
+        if(!Auth::attempt($request->only( "email","password"))){
+            return response()->json( [
                 "success"=> false,
                 "errors"=>[
                     "code"=>401,
-                    "msg"=> "No se encontraron las credenciales"
+                    "msg"=> "No se reconocen las credenciales"
                 ],
                 "data"=>"",
                 "count"=> 0
-                ], 401);
-    }
-    $user = User::where("email", $request->email)->firstOrFail();
-    $token = $user->createToken("auth_token")->plainTextToken;
+            ], 401);
 
-    return response()->json([
-        "success"=> true,
-        "errors"=>[
-            "code"=>200,
-            "msg"=> ""
-            ],
-            "data"=>[
-                "access_token"=> $token,
-                "token_type"=> "Bearer"
-                ],
-            "msg"=>"Ha iniciado sesión correctamente",
-            "count"=> 1
-            ],200);
-    }
+        }
+        $user = User::where("email", $request->email)->firstOrFail();
+        $token = $user->createToken("auth_token")->plainTextToken;
 
-    public function me(Request $request){
-        return response()->json([
+        return response()->json( [
             "success"=> true,
             "errors"=>[
                 "code"=>200,
                 "msg"=> ""
-                ],
-                "data"=>$request->user(),
-                "count"=> 1
-            ],200);
+            ],
+            "data"=>[
+                "access_token"=>$token,
+                "token_type"=> "Bearer"
+            ],
+            "count"=> 1
+        ], 200);
+    
+    
+    }
+
+    public function me(Request $request) {
+        return response()->json( [
+            "success"=> true,
+            "errors"=>[
+                "code"=>200,
+                "msg"=> ""
+            ],
+            "data"=>$request->user(),
+            "count"=> 1
+        ], 200);
     }
 }
